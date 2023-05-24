8995045833564686465b9ad98a1887d9bbd6d379
Luis Felipe <92445163+luissouzadeveloper@users.noreply.github.com>
2023-05-24 19:55:12 -0300

    Add files via upload

---
+++ aula04/index.js
@@ -0,0 +1,26 @@
+// VARIÁVEIS
+
+let nome = 'Raimundo'; // String
+
+console.log( nome + ' nasceu em 1984');
+console.log('Em 2000 ' + nome + ' conheceu Maria');
+console.log(nome + ' casou-se com Maria em 2012');
+console.log('Maria teve um filho com ' + nome + ' em 2015');
+console.log('O filho de ' + nome + ' chama-se Eduardo');
+
+let vazio; // Variável declarada mas não inicializada.
+vazio = 'Não vazio' // <- Inicializada normalmente.
+console.log(vazio); // Mesmo não inicializada, retorna-rá um valor 'undefined'!
+vazio = undefined;
+console.log(vazio);
+
+/*
+    Regras:
+
+    1- Não pode usar palavras reservadas.
+    2- As variáveis precisam ter nomes claros ou significativos!
+    3- Não pode começar nomes de variáveis com números.
+    4- As variáveis não podem conter espaços em branco ou traços.
+    5- As variáveis são case-sensitive, ou seja, minúsculas e maiúsculas fazem diferença.
+*/  
+
