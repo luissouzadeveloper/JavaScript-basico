1b43a1563adeb3da91b71de6dba2ad2d2860bc84
Luis Felipe <92445163+luissouzadeveloper@users.noreply.github.com>
2023-05-25 20:20:44 -0300

    Tipos primitivos

---
+++ aula07/index.js
@@ -0,0 +1,16 @@
+// TIPOS PRIMITIVOS
+
+const nome = 'Maria'; // <- String
+
+const numero1 = 10; // <- Number
+const numero2 = 5.5 // <- Number
+
+let nomeAluno; // <- Undefined -> Não aponta para lugar nenhum na memória
+
+const sobrenomeAluno = null; // <- Null -> Não aponta para lugar nenhum na memória
+
+let boolean = true; // <- Boolean -> Retorna dois valores! true or false
+let boolean1 = false; // <- Boolean -> Retorna dois valores! true or false
+
+console.log(typeof(boolean)); // typeof() -> Retorna o tipo de uma variável
+
