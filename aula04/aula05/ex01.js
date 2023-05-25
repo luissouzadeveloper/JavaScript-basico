e9b8c1c43cbea0296c742cb46e1ae5fff6b25623
Luis Felipe <92445163+luissouzadeveloper@users.noreply.github.com>
2023-05-25 19:25:54 -0300

    Delete aula04/aula05 directory

--- aula04/aula05/ex01.js
+++
@@ -1,8 +0,0 @@
-const nome = 'Luis Felipe';
-let idade = 23;
-let peso = 53.90;
-let altura = 1.70;
-let IMC = peso / (altura * altura);
-let nascimento = 2023 - idade - 1;
-
-console.log(`${nome} tem ${idade} anos, pesa ${peso} kg, tem ${altura} de altura e seu IMC é de ${IMC.toFixed(1)} e nasceu em ${nascimento}`)
