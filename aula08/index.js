7a35fb7ac50d8ff1ec452856e03e512fa54a1386
Luis Felipe <92445163+luissouzadeveloper@users.noreply.github.com>
2023-05-26 20:14:30 -0300

    Operadores matemáticos, atribuição e incremeneto.

---
+++ aula08/index.js
@@ -0,0 +1,41 @@
+// OPERADORES
+/*
+    Aritméticos 
+    
+    + - / * %
+*/
+/*const num1 = 10
+const num2 = 5;
+console.log('Soma = ' + (num1 + num2));
+console.log('Subtração = ' + (num1 - num2));
+console.log('Multiplicação = ' + (num1 * num2));
+console.log('Divisão = ' + (num1 / num2));
+console.log('Resto da divisão = ' + (num1 % num2));
+*/
+/*
+    Ordem de precedência
+
+    1º ()
+    2º **
+    3º * / %
+    4º + -
+*/ 
+
+// Incremento = ++ e decremento = --
+/*let contador = 1;
+console.log(contador++);
+console.log(contador);
+console.log(++contador);
+let contador2 = 10
+console.log(--contador2);
+*/
+
+let c = 1;
+c *= 10;
+console.log(c);
+
+// Conversão de tipos
+let n1 = 10;
+let n2 = Number('2.5');
+console.log(n1 + n2);
+
